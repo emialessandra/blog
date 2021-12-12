f6012efacf75c386c365cc54a002a668c08f147a
emialessandra <nohemi.piccinini@gmail.com>
2021-12-12 14:47:07 -0300

    working strapi app

---
+++ editorial-strapi-app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2268012ebf5364444182e32a209a576'),
+    },
+  },
+});
---
+++ editorial-strapi-app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8b3f56de-1ba2-4cb4-93cb-d388016c1d99'
+};
